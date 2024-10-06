a33ed4e775c718d74a59810cafca58a37f443b9b
Alex-Evo-2266 <ghbdtn2266@mail.ru>
2024-10-06 15:06:38 +0300

    _

--- src/stories/Layout/ContentBox.stories.tsx
+++ src/stories/Layout/ContentBox.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { Card, ContentBox, IconButton, TextField } from '../../lib/index';
+import { Card, ContentBox, TextField } from '../../lib/index';
 import { Trash } from 'lucide-react';
 // import React from 'react';
 
