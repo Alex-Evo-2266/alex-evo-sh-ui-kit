0b07a4b3a7a8333d033ed3848cd93cdd790018c7
Alex-Evo-2266 <ghbdtn2266@mail.ru>
2025-09-12 16:40:15 +0300

    table

--- src/lib/ui/Table/Table.tsx
+++ src/lib/ui/Table/Table.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useMemo, useRef } from "react"
+import { useMemo } from "react"
 import { Column, ITable } from "../../model/table"
 import "./Table.scss"
 import { getColumnsName } from "../../helpers/table"
@@ -13,34 +13,10 @@ interface ITableProps extends ITable{
 export const Table = ({data, columns, onDelete, onContextMenu, onEdit, actions, onClickRow, adaptive}:ITableProps) => {
 
     const cols = useMemo<Column[]>(()=>columns ?? getColumnsName(data),[data, columns])
-    const tableref = useRef<HTMLTableElement>(null)
-
-    const getMaxHeight = useCallback(() => {
-        if(!tableref.current) return;
-        const table = tableref.current
-        const body = table.tBodies.item(0)
-        const th = table.tHead?.children.item(0)?.children
-        const countCol = th?.length ?? 1
-        const maxH: number[] =  new Array(countCol).fill(0)
-        if(!body) return;
-        for(const row of body.children){
-            for(let index = 0; index < row.children.length; index++){
-                const cell = row.children[index]
-                const height = cell.getBoundingClientRect().height
-                if(maxH[index] < height)
-                    maxH[index] = height
-            }
-        }
-        return maxH
-    },[tableref])
-
-    // useEffect(()=>{
-    //     console.log(maxH)
-    // },[maxH, tableref])
 
     return(
         <div className={`table-container mt-3 ${onClickRow?"clicked":""} ${adaptive? "auto-rotate":""}`}>
-            <table ref={tableref}>
+            <table>
                 <thead>
                     <tr>
                         {
