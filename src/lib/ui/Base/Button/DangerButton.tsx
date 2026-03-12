89567e4cf4be974b98b17dd8bff76ebbf78e2b62
Alex-Evo-2266 <ghbdtn2266@mail.ru>
2026-03-12 20:17:25 +0300

    danger btn

--- src/lib/ui/Base/Button/DangerButton.tsx
+++ src/lib/ui/Base/Button/DangerButton.tsx
@@ -1,23 +1,28 @@
 import { useState } from "react"
 import { BaseDialog } from "../../Dialog/BaseDialog/BaseDialog"
 import { Button, IButtonProps } from "./Button"
+import { ModalPortal } from "../../../portal/dialog"
 
 
 
 export interface DangerButtonProps extends IButtonProps{
   header: string
   text: string
+  container?: HTMLElement | null
 }
 
-export const DangerButton:React.FC<DangerButtonProps> = ({text, header, onClick, styleType, ...props}) => {
+export const DangerButton:React.FC<DangerButtonProps> = ({container, text, header, onClick, styleType, ...props}) => {
 
-    const [isOpenDialog, setOpenDialog] = useState(false)
+    const [isOpenDialog, setOpenDialog] = useState<React.MouseEvent<HTMLButtonElement, MouseEvent> | null>(null)
 
   return(
     <>
-    <Button styleType={styleType ?? "filled"} {...props} onClick={()=>setOpenDialog(true)}/>
+    <Button styleType={styleType ?? "filled"} {...props} onClick={setOpenDialog}/>
     {
-        isOpenDialog && <BaseDialog header={header} text={text} onHide={()=>setOpenDialog(false)}/>
+        isOpenDialog !== null && 
+        <ModalPortal container={container}>
+            <BaseDialog header={header} text={text} onSuccess={()=>onClick?.(isOpenDialog)} onHide={()=>setOpenDialog(null)}/>
+        </ModalPortal>
     }
     </>
   )
