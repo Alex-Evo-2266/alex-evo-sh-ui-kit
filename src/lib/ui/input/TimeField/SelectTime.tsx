8d31b49844696d60d2ffebc9f4bfff69a9f79d44
Alex-Evo-2266 <ghbdtn2266@mail.ru>
2026-03-15 14:31:26 +0300

    time picker

--- src/lib/ui/input/TimeField/SelectTime.tsx
+++ src/lib/ui/input/TimeField/SelectTime.tsx
@@ -70,32 +70,32 @@ function Hours({setHours}: HoursProps){
 	return(
 	<div className="clock-input">
 		<div className="clock-input-am">
-			<span onClick={()=>setHours(1)}>1</span>
-			<span onClick={()=>setHours(2)}>2</span>
-			<span onClick={()=>setHours(3)}>3</span>
-			<span onClick={()=>setHours(4)}>4</span>
-			<span onClick={()=>setHours(5)}>5</span>
-			<span onClick={()=>setHours(6)}>6</span>
-			<span onClick={()=>setHours(7)}>7</span>
-			<span onClick={()=>setHours(8)}>8</span>
-			<span onClick={()=>setHours(9)}>9</span>
-			<span onClick={()=>setHours(10)}>10</span>
-			<span onClick={()=>setHours(11)}>11</span>
-			<span onClick={()=>setHours(12)}>12</span>
+			<span className="number-span" onClick={()=>setHours(1)}><span>1</span></span>
+			<span className="number-span" onClick={()=>setHours(2)}><span>2</span></span>
+			<span className="number-span" onClick={()=>setHours(3)}><span>3</span></span>
+			<span className="number-span" onClick={()=>setHours(4)}><span>4</span></span>
+			<span className="number-span" onClick={()=>setHours(5)}><span>5</span></span>
+			<span className="number-span" onClick={()=>setHours(6)}><span>6</span></span>
+			<span className="number-span" onClick={()=>setHours(7)}><span>7</span></span>
+			<span className="number-span" onClick={()=>setHours(8)}><span>8</span></span>
+			<span className="number-span" onClick={()=>setHours(9)}><span>9</span></span>
+			<span className="number-span" onClick={()=>setHours(10)}><span>10</span></span>
+			<span className="number-span" onClick={()=>setHours(11)}><span>11</span></span>
+			<span className="number-span" onClick={()=>setHours(12)}><span>12</span></span>
 		</div>
 		<div className="clock-input-pm">
-			<span onClick={()=>setHours(13)}>13</span>
-			<span onClick={()=>setHours(14)}>14</span>
-			<span onClick={()=>setHours(15)}>15</span>
-			<span onClick={()=>setHours(16)}>16</span>
-			<span onClick={()=>setHours(17)}>17</span>
-			<span onClick={()=>setHours(18)}>18</span>
-			<span onClick={()=>setHours(19)}>19</span>
-			<span onClick={()=>setHours(20)}>20</span>
-			<span onClick={()=>setHours(21)}>21</span>
-			<span onClick={()=>setHours(22)}>22</span>
-			<span onClick={()=>setHours(23)}>23</span>
-			<span onClick={()=>setHours(0)}>0</span>
+			<span className="number-span" onClick={()=>setHours(13)}><span>13</span></span>
+			<span className="number-span" onClick={()=>setHours(14)}><span>14</span></span>
+			<span className="number-span" onClick={()=>setHours(15)}><span>15</span></span>
+			<span className="number-span" onClick={()=>setHours(16)}><span>16</span></span>
+			<span className="number-span" onClick={()=>setHours(17)}><span>17</span></span>
+			<span className="number-span" onClick={()=>setHours(18)}><span>18</span></span>
+			<span className="number-span" onClick={()=>setHours(19)}><span>19</span></span>
+			<span className="number-span" onClick={()=>setHours(20)}><span>20</span></span>
+			<span className="number-span" onClick={()=>setHours(21)}><span>21</span></span>
+			<span className="number-span" onClick={()=>setHours(22)}><span>22</span></span>
+			<span className="number-span" onClick={()=>setHours(23)}><span>23</span></span>
+			<span className="number-span" onClick={()=>setHours(0)}><span>0</span></span>
 		</div>
 		<span className="center"></span>
 	</div>
@@ -106,18 +106,18 @@ function Minuts({setMinutes}: MinutesProps){
 	return(
 	<div className="clock-input">
 		<div className="clock-input-minute">
-			<span onClick={()=>setMinutes(0)}>00</span>
-			<span onClick={()=>setMinutes(5)}>05</span>
-			<span onClick={()=>setMinutes(10)}>10</span>
-			<span onClick={()=>setMinutes(15)}>15</span>
-			<span onClick={()=>setMinutes(20)}>20</span>
-			<span onClick={()=>setMinutes(25)}>25</span>
-			<span onClick={()=>setMinutes(30)}>30</span>
-			<span onClick={()=>setMinutes(35)}>35</span>
-			<span onClick={()=>setMinutes(40)}>40</span>
-			<span onClick={()=>setMinutes(45)}>45</span>
-			<span onClick={()=>setMinutes(50)}>50</span>
-			<span onClick={()=>setMinutes(55)}>55</span>
+			<span className="number-span" onClick={()=>setMinutes(0)}><span>00</span></span>
+			<span className="number-span" onClick={()=>setMinutes(5)}><span>05</span></span>
+			<span className="number-span" onClick={()=>setMinutes(10)}><span>10</span></span>
+			<span className="number-span" onClick={()=>setMinutes(15)}><span>15</span></span>
+			<span className="number-span" onClick={()=>setMinutes(20)}><span>20</span></span>
+			<span className="number-span" onClick={()=>setMinutes(25)}><span>25</span></span>
+			<span className="number-span" onClick={()=>setMinutes(30)}><span>30</span></span>
+			<span className="number-span" onClick={()=>setMinutes(35)}><span>35</span></span>
+			<span className="number-span" onClick={()=>setMinutes(40)}><span>40</span></span>
+			<span className="number-span" onClick={()=>setMinutes(45)}><span>45</span></span>
+			<span className="number-span" onClick={()=>setMinutes(50)}><span>50</span></span>
+			<span className="number-span" onClick={()=>setMinutes(55)}><span>55</span></span>
 		</div>
 		<span className="center"></span>
 	</div>
